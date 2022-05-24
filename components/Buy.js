47fbeaf22d61c96a269a6ff1282a621bf210e244
Fábio Matias <fabio_mcm2@hotmail.com>
2022-05-24 23:22:58 +0100

    Check tx status

--- components/Buy.js
+++ components/Buy.js
@@ -1,18 +1,24 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { Keypair, Transaction } from "@solana/web3.js";
+import { findReference, FindReferenceError } from "@solana/pay";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { InfinitySpin } from "react-loader-spinner";
 import IPFSDownload from "./IpfsDownload";
+
+const STATUS = {
+  Initial: "Initial",
+  Submitted: "Submitted",
+  Paid: "Paid"
+};
 
 export default function Buy({ itemID }) {
   const { connection } = useConnection();
   const { publicKey, sendTransaction } = useWallet();
   const orderID = useMemo(() => Keypair.generate().publicKey, []); // Public key used to identify the order
 
-  const [paid, setPaid] = useState(null);
-  const [loading, setLoading] = useState(false); // Loading state of all above
+  const [loading, setLoading] = useState(false);
+  const [status, setStatus] = useState(STATUS.Initial); // Tracking transaction status
 
-  // useMemo is a React hook that only computes the value if the dependencies change
   const order = useMemo(
     () => ({
       buyer: publicKey.toString(),
@@ -22,7 +28,6 @@ export default function Buy({ itemID }) {
     [publicKey, orderID, itemID]
   );
 
-  // Fetch the transaction object from the server
   const processTransaction = async () => {
     setLoading(true);
     const txResponse = await fetch("../api/createTransaction", {
@@ -34,25 +39,53 @@ export default function Buy({ itemID }) {
     });
     const txData = await txResponse.json();
 
-    // We create a transaction object
     const tx = Transaction.from(Buffer.from(txData.transaction, "base64"));
     console.log("Tx data is", tx);
 
-    // Attempt to send the transaction to the network
     try {
-      // Send the transaction to the network
       const txHash = await sendTransaction(tx, connection);
       console.log(
         `Transaction sent: https://solscan.io/tx/${txHash}?cluster=devnet`
       );
-      // Even though this could fail, we're just going to set it to true for now
-      setPaid(true);
+      setStatus(STATUS.Submitted);
     } catch (error) {
       console.error(error);
     } finally {
       setLoading(false);
     }
   };
+
+  useEffect(() => {
+    // Check if transaction was confirmed
+    if (status === STATUS.Submitted) {
+      setLoading(true);
+      const interval = setInterval(async () => {
+        try {
+          const result = await findReference(connection, orderID);
+          console.log("Finding tx reference", result.confirmationStatus);
+          if (
+            result.confirmationStatus === "confirmed" ||
+            result.confirmationStatus === "finalized"
+          ) {
+            clearInterval(interval);
+            setStatus(STATUS.Paid);
+            setLoading(false);
+            alert("Thank you for your purchase!");
+          }
+        } catch (e) {
+          if (e instanceof FindReferenceError) {
+            return null;
+          }
+          console.error("Unknown error", e);
+        } finally {
+          setLoading(false);
+        }
+      }, 1000);
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  }, [status]);
 
   if (!publicKey) {
     return (
@@ -68,7 +101,7 @@ export default function Buy({ itemID }) {
 
   return (
     <div>
-      {paid ? (
+      {status === STATUS.Paid ? (
         <IPFSDownload
           filename="emojis.zip"
           hash="QmWWH69mTL66r3H8P4wUn24t1L5pvdTJGUTKBqT11KCHS5"
