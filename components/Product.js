2a17750594d5a4fa1ab0dd1a862e76961dcd2299
Fábio Matias <fabio_mcm2@hotmail.com>
2022-05-24 22:44:46 +0100

    Perform transaction with SOL

--- components/Product.js
+++ components/Product.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "../styles/Product.module.css";
-import IPFSDownload from "./IpfsDownload";
+import Buy from "./Buy";
 
 export default function Product({ product }) {
   const { id, name, price, description, image_url } = product;
@@ -19,12 +19,7 @@ export default function Product({ product }) {
 
         <div className={styles.product_action}>
           <div className={styles.product_price}>{price} USDC</div>
-          {/* I'm hardcoding these for now, we'll fetch the hash from the API later*/}
-          <IPFSDownload
-            filename="emojis.zip"
-            hash="QmWWH69mTL66r3H8P4wUn24t1L5pvdTJGUTKBqT11KCHS5"
-            cta="Download emojis"
-          />
+            <Buy itemID={id} />
         </div>
       </div>
     </div>
